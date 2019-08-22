91ce861700288e598e6e5ec32cf4739845cb9742
yunyun <di5999@exs-mobile.com>
2019-08-22 11:12:51 +0900

    Initial Commit

---
+++ YunTalk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YunTalk
+//
+//  Created by exs-mobile yun on 22/08/2019.
+//  Copyright © 2019 Seung Yun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
